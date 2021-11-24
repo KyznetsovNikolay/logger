6e69a4c6891381f2b23ba90e216ec7c5da683f58
Nikolay Kyznetsov <kyznetsovnic@gmail.com>
2021-11-24 19:33:12 +0300

    Changed select.

--- core/Base/Model.php
+++ core/Base/Model.php
@@ -15,11 +15,13 @@ abstract class Model
     public static function find($offset, $perPage, array $params = []): array
     {
         $dbh = DataBase::getInstance();
+        $sortBy = strtoupper($params['ts']);
 
-        if ($params['type']) {
+        if ($params['type'] && $params['ts']) {
+            $stmt = $dbh->prepare("SELECT * FROM " . static::table() . " WHERE type = ${params['type']} ORDER BY ts ${sortBy} LIMIT ${offset}, ${perPage}");
+        } else if ($params['type']) {
             $stmt = $dbh->prepare("SELECT * FROM " . static::table() . " WHERE type = ${params['type']} LIMIT ${offset}, ${perPage}");
         } else if ($params['ts']) {
-            $sortBy = strtoupper($params['ts']);
             $stmt = $dbh->prepare("SELECT * FROM " . static::table() . " ORDER BY ts ${sortBy} LIMIT ${offset}, ${perPage}");
         } else {
             $stmt = $dbh->prepare("SELECT * FROM " . static::table() . " LIMIT ${offset}, ${perPage}");
